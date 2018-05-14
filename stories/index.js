1129f44d0aad848e7f034e93720ec90830374887
Jason Henriksen <jason.henriksen@gmail.com>
2018-05-14 01:25:27 -0700

    added build to github page support.  Not yet automated.

--- stories/index.js
+++ stories/index.js
@@ -20,31 +20,33 @@ var smData = TestNameTool(simpleMathParametersRaw); // name each test after it's
 
 storiesOf('Introduction', module)
   .add('What is react-factorial-test?', () => (<div>Intro Page</div>))
-  .add('How do we define parameter options?', () => (<div>Intro Page</div>))
-  .add('How do multiply possible options to make tests?', () => (<div>Intro Page</div>))
-  .add('What are the test focus buttons for?', () => (<div>Intro Page</div>))
+  .add('How do we define parameters?', () => (<div>Intro Page</div>))
+  .add('How to multiply parameters and tests?', () => (<div>Intro Page</div>))
+  .add('What are test focus buttons for?', () => (<div>Intro Page</div>))
   .add('Conclusion', () => (<div>Intro Page</div>))
 
-storiesOf('Example - SimpleMath Component', module)
+storiesOf('Example - SimpleMath', module)
     .add('Introduction', () => (<div>
-      <h2>SimpleMath</h2>
+      <hr /><div style={{ fontSize: '1.5em' }}>SimpleMath - Introduction</div><hr/>
       This view component is intended to display the sum and product of three numbers.<br/>
     For example:<SimpleMath a='5' b='2' c='3'/><i>NOTE! There is a defect in the summation!  We can use these tests to fix it.</i><br/><br/>
     It should not display items that are not supplied:<SimpleMath a='5' /><br/>
     It should gracefully show error data:<SimpleMath a='gazelle' /><br/>
     </div>))
-    .add('Parameter Documentation', () => (<ParameterDisplay name='SimpleMath' 
+    .add('Parameter Documentation', () => (<ParameterDisplay name='SimpleMath - Parameters' 
                                             description={
                                               <div>
-                                              SimpleMath is a display only component to show only the provided numbers, their sum and their product.<br/>
+                                              <b>Intro</b><br/>SimpleMath is a display only component to show only the provided numbers, their sum and their product.<br/>
                                               SimpleMath takes three parameters: a, b and c.<br/><br/>
-                                              In this test suite each of the three parameters has a set of Normal values and a bunch of Evil values.<br/>
+                                              <b>Layout</b><br />In this test suite each of the three parameters has a set of Normal values and a bunch of Evil values.<br/>
                                               This lets us check that the comopnent behaves well under stress,<br/>
                                               It also lets us watch the normal behavior without getting buried in error case handling.<br/><br/>
                                               Each parameter has 3 normal tests defined.<br/>
                                               When you take each of the 3 parameters and run each parameter's 3 normal tests, you'll get 9 tests total. (3+3+3)<br/>
                                               But when you run each parameter's values against each of the other parameter's values, you'll get 27 tests ouput. (3*3*3)<br/><br/>
                                               This parameter multiplication lets you very quickly define a comprehensive test suite for your component.<br/><br/>
+                                              <b>Data Location</b><br />
+                                              The parameters are defined in <pre>src\exampleComponentsToTest\simpleMath\SimpleMathParameters.jsx</pre><br/>
                                               Click each test name to drill down to the parameters that the test supplies.
                                               </div>
                                             }
@@ -57,7 +59,7 @@ storiesOf('Example - SimpleMath Component', module)
       [ [ smData.ATest.ANormalTest ] ]}
     />))
   
-    .add('Show tests A, then B, then C.  Normal data only', () => (
+    .add('Show A+B+C. Normal data only', () => (
       <MultiTest
         target={<SimpleMath/>}
         test={
@@ -66,7 +68,7 @@ storiesOf('Example - SimpleMath Component', module)
           [ smData.CTest.CNormalTest ],
         ]}
       />))
-    .add('A*B*C.  Normal data only', () => (
+    .add('Show A*B*C. Normal data only', () => (
       <MultiTest
         target={<SimpleMath/>}
         test={
